d61af4be0c1aa3ba6d5d4e1275e6acbb67e518c2
schneis <zabador2000@gmail.com>
2015-07-18 18:29:40 -0400

    Move form validation to a util

--- app/src/main/java/com/hoodbluck/authum/activities/LoginActivity.java
+++ app/src/main/java/com/hoodbluck/authum/activities/LoginActivity.java
@@ -3,11 +3,14 @@ package com.hoodbluck.authum.activities;
 import android.widget.EditText;
 
 import com.hoodbluck.authum.R;
+import com.hoodbluck.authum.utils.ViewUtil;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
 
 /**
  * Created on 7/18/15.
@@ -32,4 +35,13 @@ public class LoginActivity extends BaseActivity{
         RegisterActivity_.intent(this)
                 .start();
     }
+
+    @Click(R.id.login_button)
+    public void onLoginButtonClick() {
+        ArrayList<EditText> editTextList = new ArrayList<>();
+        editTextList.add(mEmailInput);
+        editTextList.add(mPasswordInput);
+        ViewUtil.isFormValid(this, editTextList);
+    }
+
 }
