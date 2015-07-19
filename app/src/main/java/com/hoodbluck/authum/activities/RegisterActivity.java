4c2e1a94eac6b1ba4f1417925ad6cd619dc32a3a
schneis <zabador2000@gmail.com>
2015-07-19 10:03:46 -0400

    Close app after creating patter

--- app/src/main/java/com/hoodbluck/authum/activities/RegisterActivity.java
+++ app/src/main/java/com/hoodbluck/authum/activities/RegisterActivity.java
@@ -90,7 +90,7 @@ public class RegisterActivity extends BaseActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 PatternRegistrationActivity_.intent(RegisterActivity.this).start();
-
+                                RegisterActivity.this.finish();
                             }
                         });
                     }
